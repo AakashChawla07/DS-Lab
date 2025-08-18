4aa6c083e45e10d20a8b3a07a0dedd2739228d11
AakashChawla07 <aakashchawla0701@gmail.com>
2025-08-18 12:45:36 +0530

    Made changes

--- Assignment 2/4.cpp
+++ Assignment 2/4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-string concat(string s1,string s2){
+string concat(string &s1,string &s2){
     return s1+s2;
 }
 string reverse(string s){
@@ -19,7 +19,29 @@ string delete_vowels(string s){
         i++;
     }
     s[j] = '\0';
+    return s;
 }
+void string_sort(string arr[],int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if (arr[j] >arr[j + 1]) {
+                string temp =arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void to_upper(){
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    if (ch >= 'A' && ch <= 'Z')
+        ch = ch + 32;
+    cout << "Lowercase character: " << ch << endl;
+
+}
+
 int main() {
     string s1,s2;
     cout << "Enter Strings: ";
@@ -30,6 +52,16 @@ int main() {
     cout<<"Concatenated Strings : "<< concat(s1,s2)<<endl;
     cout<<"Reversed String: "<<reverse(s1)<<endl;
     cout<<"String with deleted Vowels : "<<delete_vowels(s1)<<endl;
+
+    string s[] = {"Hello", "How are", "you.", "This is", "a sample", "strings array"};
+
+    string_sort(s,6);
+    cout<<"Sorted Strings :";
+    for(string str :s)
+    cout<<str<<" ";
+    cout<<endl;
+
+    to_upper();
     
 
 }
